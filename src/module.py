c52ac7661fc4cea8564650bdaf4d61668e45ba3f
Tangb <tanguy.bonneau@gmail.com>
2019-04-08 12:25:17 +0200

    Implements application creation skel

--- src/module.py
+++ src/module.py
@@ -3,53 +3,204 @@
 
 import sys
 import os
-import click
 from console import Console
 import logging
 import config
+import shutil
 
-@click.group()
-def mod():
-    pass
-
-@mod.command()
-@click.option('--mod', required=True, help='Module name.')
-def modsync(mod):
+class Module():
     """
-    Copy specified module content to valid execution location
+    Module class
     """
-    if not os.path.exists(os.path.join(config.MODULES_SRC, mod)):
-        sys.exit('Module "%s" doesn\'t exist [%s]' % (mod, os.path.join(config.MODULES_SRC, mod)))
 
-    if not os.path.exists(os.path.join(config.MODULES_DST, mod)):
-        os.mkdir(os.path.join(config.MODULES_DST, mod))
-
-    mod_backend_src = os.path.join(config.MODULES_SRC, mod, 'backend/')
-    mod_backend_dst = os.path.join(config.MODULES_DST, mod)
-    mod_frontend_src = os.path.join(config.MODULES_SRC, mod, 'frontend/')
-    mod_frontend_dst = os.path.join(config.MODULES_HTML_DST, mod)
-    mod_scripts_src = os.path.join(config.MODULES_SRC, mod, 'scripts/')
-    mod_scripts_dst = os.path.join(config.MODULES_SCRIPTS_DST, mod)
-
-    c = Console()
-    cmd = """
-if [ -d "%(BACKEND_SRC)s" ]; then
-    rsync -av "%(BACKEND_SRC)s" "%(BACKEND_DST)s"
-fi
-if [ -d "%(FRONTEND_SRC)s" ]; then
-    rsync -av "%(FRONTEND_SRC)s" "%(FRONTEND_DST)s"
-fi
-if [ -d "%(SCRIPTS_SRC)s" ]; then
-    rsync -av "%(SCRIPTS_SRC)s" "%(SCRIPTS_DST)s"
-fi
-    """ % {
-        'BACKEND_SRC': mod_backend_src, 'BACKEND_DST': mod_backend_dst,
-        'FRONTEND_SRC': mod_frontend_src, 'FRONTEND_DST': mod_frontend_dst,
-        'SCRIPTS_SRC': mod_scripts_src, 'SCRIPTS_DST': mod_scripts_dst
+    DESC_SKEL = """{
+    "icon": "help-circle-outline",
+    "global": {
+        "js": ["%(MODULE_NAME)s.service.js"],
+        "html": [],
+        "css": []
+    },
+    "config": {
+        "js": ["%(MODULE_NAME)s.config.js"],
+        "html": ["%(MODULE_NAME)s.config.html"]
     }
-    logging.debug('modsync resp: %s' % cmd)
-    resp = c.command(cmd, 15)
-    if resp['error'] or resp[u'killed']:
-        logging.error(u'Error occured while sync module content: %s' % (u'killed' if resp[u'killed'] else resp[u'stderr']))
+}
+    """
+    ANGULAR_SERVICE_SKEL = """/**
+ * %(MODULE_NAME_CAPITALIZED)s service
+ * Handle %(MODULE_NAME)s application requests
+ */
+var %(MODULE_NAME)sService = function(\$q, \$rootScope, rpcService) {
+    var self = this;
 
+    /**
+     * Catch x.x.x events
+     */
+    \$rootScope.\$on('x.x.x', function(event, uuid, params) {
+    });
+}
+    
+var RaspIot = angular.module('RaspIot');
+RaspIot.service('%(MODULE_NAME)sService', ['\$q', '\$rootScope', 'rpcService', %(MODULE_NAME)sService]);
+    """
+    ANGULAR_CONTROLLER_SKEL = """/**
+ * %(MODULE_NAME_CAPITALIZED)s config directive
+ * Handle %(MODULE_NAME)s application configuration
+ */
+var %(MODULE_NAME)sConfigDirective = function(\$rootScope, %(MODULE_NAME)sService, raspiotService) {
+
+    var %(MODULE_NAME)sConfigController = function() {
+        var self = this;
+
+        /**
+         * Init controller
+         */
+        self.init = function() {
+            // TODO
+        };
+    };
+
+    var %(MODULE_NAME)sConfigLink = function(scope, element, attrs, controller) {
+        controller.init();
+    };
+
+    return {
+        templateUrl: '%(MODULE_NAME)s.config.html',
+        replace: true,
+        scope: true,
+        controller: %(MODULE_NAME)sConfigController,
+        controllerAs: '%(MODULE_NAME)sCtl',
+        link: %(MODULE_NAME)sConfigLink
+    };
+};
+
+var RaspIot = angular.module('RaspIot');
+RaspIot.directive('%(MODULE_NAME)sConfigDirective', ['\$rootScope', '%(MODULE_NAME)sService', 'raspiotService', %(MODULE_NAME)sConfigDirective]);
+    """
+    ANGULAR_CONTROLLER_TEMPLATE_SKEL = """<div layout="column" layout-padding ng-cloak>
+
+    <md-list>
+    </md-list>
+
+</div>
+    """
+    PYTHON_MODULE_SKEL = """#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from raspiot.utils import MissingParameter, InvalidParameter, CommandError
+from raspiot.raspiot import RaspIotModule
+
+class %(MODULE_NAME_CAPITALIZED)s(RaspIotModule):
+    \\"\\"\\"
+    %(MODULE_NAME_CAPITALIZED)s application
+    \\"\\"\\"
+    MODULE_AUTHOR = u'TODO'
+    MODULE_VERSION = u'0.0.0'
+    MODULE_DEPS = []
+    MODULE_DESCRIPTION = u'TODO'
+    MODULE_LONGDESCRIPTION = u'TODO'
+    MODULE_TAGS = []
+    MODULE_CATEGORY = u'TODO'
+    MODULE_COUNTRY = None
+    MODULE_URLINFO = None
+    MODULE_URLHELP = None
+    MODULE_URLSITE = None
+    MODULE_URLBUGS = None
+
+    MODULE_CONFIG_FILE = u'%(MODULE_NAME)s.conf'
+    DEFAULT_CONFIG = {}
+
+    def __init__(self, bootstrap, debug_enabled):
+        \\"\\"\\"
+        Constructor
+
+        Params:
+            bootstrap (dict): bootstrap objects
+            debug_enabled: debug status
+        \\"\\"\\"
+        RaspIotModule.__init__(self, bootstrap, debug_enabled)
+
+    def _configure(self):
+        \\"\\"\\"
+        Configure module
+        \\"\\"\\"
+        # launch here custom thread or action that takes time to process
+        pass
+
+    def _stop(self):
+        \\"\\"\\"
+        Stop module
+        \\"\\"\\"
+        # stop here your custom threads or close external connections
+        pass
+
+    def event_received(self, event):
+        \\"\\"\\"
+        Event received
+
+        Params:
+            event (MessageRequest): event data
+        \\"\\"\\"
+        # execute here actions when you receive an event:
+        #  - on time event => cron task
+        #  - on alert event => send email or push message
+        #  - ...
+        pass
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def create(self, module_name):
+        """
+        Create module skeleton
+
+        Args:
+            module_name (string): module name
+        """
+        path = os.path.join(config.MODULES_SRC, module_name)
+        self.logger.info('Creating module "%s" in "%s"' % (module_name, path))
+        
+        if os.path.exists(path):
+            self.logger.error('Module "%s" already exists in "%s"' % (module_name, path))
+            return False
+
+        templates = {
+            'ANGULAR_SERVICE': self.ANGULAR_SERVICE_SKEL % {'MODULE_NAME': module_name, 'MODULE_NAME_CAPITALIZED': module_name.capitalize()},
+            'ANGULAR_CONTROLLER': self.ANGULAR_CONTROLLER_SKEL % {'MODULE_NAME': module_name, 'MODULE_NAME_CAPITALIZED': module_name.capitalize()},
+            'ANGULAR_CONTROLLER_TEMPLATE': self.ANGULAR_CONTROLLER_TEMPLATE_SKEL % {'MODULE_NAME': module_name, 'MODULE_NAME_CAPITALIZED': module_name.capitalize()},
+            'DESC': self.DESC_SKEL % {'MODULE_NAME': module_name, 'MODULE_NAME_CAPITALIZED': module_name.capitalize()},
+            'PYTHON_MODULE': self.PYTHON_MODULE_SKEL % {'MODULE_NAME': module_name, 'MODULE_NAME_CAPITALIZED': module_name.capitalize()}
+        }
+
+        c = Console()
+        resp = c.command("""
+/bin/mkdir -p %(MODULE_DIR)s/backend
+/usr/bin/touch %(MODULE_DIR)s/backend/__init__.py
+/bin/echo "%(PYTHON_MODULE)s" > %(MODULE_DIR)s/backend/%(MODULE_NAME)s.py
+/bin/mkdir -p %(MODULE_DIR)s/frontend
+/bin/echo "%(DESC)s" > %(MODULE_DIR)s/frontend/desc.json
+/bin/echo "%(ANGULAR_SERVICE)s" > %(MODULE_DIR)s/frontend/%(MODULE_NAME)s.service.js
+/bin/echo "%(ANGULAR_CONTROLLER)s" > %(MODULE_DIR)s/frontend/%(MODULE_NAME)s.config.js
+/bin/echo "%(ANGULAR_CONTROLLER_TEMPLATE)s" > %(MODULE_DIR)s/frontend/%(MODULE_NAME)s.config.html
+/bin/mkdir -p %(MODULE_DIR)s/tests
+/usr/bin/touch %(MODULE_DIR)s/tests/__init__.py
+        """ % {
+            'MODULE_DIR': path,
+            'MODULE_NAME': module_name,
+            'DESC': templates['DESC'],
+            'ANGULAR_SERVICE': templates['ANGULAR_SERVICE'],
+            'ANGULAR_CONTROLLER': templates['ANGULAR_CONTROLLER'],
+            'ANGULAR_CONTROLLER_TEMPLATE': templates['ANGULAR_CONTROLLER_TEMPLATE'],
+            'PYTHON_MODULE': templates['PYTHON_MODULE']
+        }, 10)
+        if resp['error'] or resp['killed']:
+            self.logger.error('Error occured while pulling repository: %s' % ('killed' if resp['killed'] else resp['stderr']))
+            shutil.rmtree(path)
+            return False
+        
+        return True
 
