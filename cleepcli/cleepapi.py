59177455f394a57c717b8a0a24dec14a56c82523
Tangb <tanguy.bonneau@gmail.com>
2019-05-01 19:27:37 +0200

    Fix bug in cleepapi after urllib migration

--- cleepcli/cleepapi.py
+++ cleepcli/cleepapi.py
@@ -48,7 +48,7 @@ class CleepApi():
         resp_data = None
         try:
             data_json = json.dumps(data).encode('utf-8')
-            resp = self.http.request('POST', url, body=data_json, headers={'Content-Type': 'application/json-rpc'})
+            resp = self.http.request('POST', url, body=data_json, headers={'Content-Type': 'application/json'})
             resp_data = json.loads(resp.data.decode('utf-8'))
             self.logger.debug('Response[%s]: %s' % (resp.status, resp_data))
         except Exception as e:
